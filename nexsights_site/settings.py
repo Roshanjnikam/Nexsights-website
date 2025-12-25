ee3201a1e9787a57e3442b4810e088e1da32b1d7
Roshan Nikam <roshanjnikam075@gmail.com>
2025-12-25 13:42:04 +0530

    Fix static files config and recollect CSS

--- nexsights_site/settings.py
+++ nexsights_site/settings.py
@@ -122,10 +122,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 
+# ================= STATIC FILES =================
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 
